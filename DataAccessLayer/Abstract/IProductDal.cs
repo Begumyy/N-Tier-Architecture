9f0eae9e11340da55af365b65ec29a2620aebec8
Begüm Yünkül <yunkulbegum@gmail.com>
2024-03-14 00:47:32 +0300

    abstract ve concrete dosyalar oluşturuldu, repository tanımlandı.

---
+++ DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IProductDal:IGenericDal<Product>
+    { 
+    }
+}
+//Dal --> Data Access Layer'den geliyor.
